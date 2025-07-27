7a4bb177638b92e88f690e144439fd92dcacf4a9
Jared Bentil <jaredbentil@gmail.com>
2025-07-27 01:09:30 -0400

    first commit to temp conversion

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -21,8 +21,8 @@
 #Remember to access global variables using the global keyword if you need to modify them inside functions. However, for this task, you’ll only be reading their values.
 #Use input validation to ensure that the user enters a valid temperature and unit.
 
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_celsius(fahrenheit):
     """Convert Fahrenheit to Celsius."""
