fa335af3b4b58bfd849b7d7ed4cb6740e32628ca
Jared Bentil <jaredbentil@gmail.com>
2025-08-09 23:57:49 -0400

    first commit

--- oop/library_system.py
+++ oop/library_system.py
@@ -37,7 +37,7 @@ class EBook(Book):
         self.file_size = file_size
 
     def __str__(self):
-        return f"Ebook: {super().__str__()}, File Size: {self.file_size}"  
+        return f"Ebook: {super().__str__()}, File Size: {self.file_size}KB"  
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
