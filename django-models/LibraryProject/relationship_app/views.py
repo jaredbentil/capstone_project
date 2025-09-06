89b4bca6e4f583107fb8b252155bab94c79b71c9
Jared Bentil <jaredbentil@gmail.com>
2025-09-05 23:28:16 -0400

    first commit

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,12 +7,13 @@ from .models import Book, Library
 # -------------------------------
 def list_books(request):
     books = Book.objects.all()
-    return render(request, "list_books.html", {"books": books})
+    # Explicit template path
+    return render(request, "relationship_app/list_books.html", {"books": books})
 
 # -------------------------------
 # Class-based view
 # -------------------------------
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = "library_detail.html"
+    template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
