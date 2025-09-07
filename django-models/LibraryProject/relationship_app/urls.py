5d1bb3398ef2a640240a10eeb36bfcc6d4a0e066
Jared Bentil <jaredbentil@gmail.com>
2025-09-07 07:52:09 -0400

    third commit

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -23,8 +23,8 @@ urlpatterns = [
     path("member-view/", views.member_view, name="member_view"),
 
     # ✅ Book management URLs
-    path("books/add/", views.add_book, name="add_book"),
-    path("books/<int:pk>/edit/", views.edit_book, name="edit_book"),
+    path("books/add/", views.add_book, name="add_book/"),
+    path("books/<int:pk>/edit/", views.edit_book, name="edit_book/"),
     path("books/<int:pk>/delete/", views.delete_book, name="delete_book"),
 ]
 
