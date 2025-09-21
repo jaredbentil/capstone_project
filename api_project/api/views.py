5dd231c9361060f4bc07674dcd9e4f17b2a34ca2
Jared Bentil <jaredbentil@gmail.com>
2025-09-20 23:59:56 -0400

    Authentication

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -9,3 +9,4 @@ from .serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
