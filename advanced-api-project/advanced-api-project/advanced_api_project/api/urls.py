7e47a7811be1d146ab1235049dae3e3a7f96fc72
Jared Bentil <jaredbentil@gmail.com>
2025-09-27 22:09:12 -0400

    updated api.views

--- advanced-api-project/advanced-api-project/advanced_api_project/api/urls.py
+++ advanced-api-project/advanced-api-project/advanced_api_project/api/urls.py
@@ -1,6 +1,7 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
 from api import views
+from .views import BookListCreate, BookRetrieveUpdateDestroy
 
 router = DefaultRouter()
 router.register(r'authors', views.AuthorViewSet)
@@ -8,5 +9,7 @@ router.register(r'books', views.BookViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    
+    path('books/', BookListCreate.as_view(), name='book-list-create'),
+    path('books/<int:pk>/', BookRetrieveUpdateDestroy.as_view(), name='book-detail-update-delete'),
+         
 ]
--- advanced-api-project/advanced-api-project/advanced_api_project/api/views.py
+++ advanced-api-project/advanced-api-project/advanced_api_project/api/views.py
@@ -1,15 +1,42 @@
-from django.shortcuts import render
+# api/views.py (The Recommended Corrected Version)
 
-# Create your views here.
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from .models import Author, Book
 from .serializers import AuthorSerializer, BookSerializer
 
+# --- ViewSet for Author (Handles List, Create, Retrieve, Update, Destroy) ---
 class AuthorViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows authors to be viewed or edited.
+    Requires authentication for creation/modification.
+    """
     queryset = Author.objects.all()
-    serializer_class = AuthorSerializer 
+    serializer_class = AuthorSerializer
+    # Allow anyone to view (GET), but require authentication to create/update/delete.
+    permission_classes = [IsAuthenticatedOrReadOnly] 
+    
+    # NOTE: No need to define separate list/detail/create methods, ModelViewSet does it for you.
 
+
+# --- ViewSet for Book (Handles List, Create, Retrieve, Update, Destroy) ---
 class BookViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows books to be viewed or edited.
+    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    
+    # Customization Example: Filter books by author if a query parameter is provided
+    # ModelViewSet uses get_queryset() just like the generic views
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        author_name = self.request.query_params.get('author', None)
+        
+        if author_name is not None:
+            # Assumes 'Author' model has a 'name' field
+            # Filters books where the author's name contains the query string (case-insensitive)
+            queryset = queryset.filter(author__name__icontains=author_name)
+            
+        return queryset
