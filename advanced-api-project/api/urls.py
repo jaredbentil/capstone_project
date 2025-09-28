53795190b1b60ca1da75f48fd3b0bdd612416d3d
Jared Bentil <jaredbentil@gmail.com>
2025-09-28 15:42:01 -0400

    feat: view set and superuser

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('books/', views.BookListView.as_view(), name='book-list'),
     path('books/create/', views.BookCreateView.as_view(), name='book-create'),
     path('books/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),
-    path('books/<int:pk>/update/', views.BookUpdateView.as_view(), name='book-update'),
-    path('books/<int:pk>/delete/', views.BookDeleteView.as_view(), name='book-delete'),
+    path('books/update/', views.BookUpdateView.as_view(), name='book-update'),
+    path('books/delete/', views.BookDeleteView.as_view(), name='book-delete'),
 ]
 
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,16 +2,19 @@ from rest_framework import generics, permissions, viewsets
 from .models import Author, Book
 from .serializers import AuthorSerializer, BookSerializer
 
-
+# --- Author ViewSet 
 class AuthorViewSet(viewsets.ModelViewSet):
-   
+    """
+    API endpoint that allows authors to be viewed or edited.
+    """
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
+
 class BookListView(generics.ListAPIView):
-   
+    
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.AllowAny]
@@ -25,7 +28,7 @@ class BookDetailView(generics.RetrieveAPIView):
 
 
 class BookCreateView(generics.CreateAPIView):
-   
+    
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.IsAuthenticated]
@@ -39,8 +42,7 @@ class BookUpdateView(generics.UpdateAPIView):
 
 
 class BookDeleteView(generics.DestroyAPIView):
-   
+    
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.IsAuthenticated]
-
