8c8f6955a0d42b782ba031823f46d957062f1c0b
Jared Bentil <jaredbentil@gmail.com>
2025-10-04 14:50:44 -0400

    initial blod settings

---
+++ django_blog/blog/static/blog/js/script.js
@@ -0,0 +1,4 @@
+// A simple script to confirm that the JS file is loaded
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed. Django blog is ready!');
+});
