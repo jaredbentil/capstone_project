fc61ace56fb852105a7fc5310eff8baf748d2ec0
Jared Bentil <jaredbentil@gmail.com>
2025-10-04 21:44:41 -0400

    Feature: added user profiles

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    
+    # Using our custom registration view
+    path('register/', views.register, name='register'),
+
+    # Using Django's built-in Login and Logout views
+    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
+
+    # Using our custom profile view
+    path('profile/', views.profile, name='profile'),
 ]
