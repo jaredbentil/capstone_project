674ff18c737d3df45689bb7f5f6cba67956f3f3f
Jared Bentil <jaredbentil@gmail.com>
2025-10-05 08:02:48 -0400

    Feature: tags

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,7 +1,18 @@
 from django import forms
+from .models import Post, Comment
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from .models import Comment
+from django.contrib.auth.forms import UserCreationForm
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'tags']
+        widgets = {
+            'tags': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter tags separated by commas'
+            }),
+        }
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
