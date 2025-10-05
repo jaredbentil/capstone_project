21fd2e985c256e215fb3be9f6d195b0624374151
Jared Bentil <jaredbentil@gmail.com>
2025-10-04 21:07:23 -0400

    corrections made to  postgresql

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -76,9 +76,14 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_blog_db',     # The database created
+        'USER': 'blog_user',         # The user you created
+        'PASSWORD': 'th3Newgigabit', # The password set
+        'HOST': 'localhost',         # Or '127.0.0.1'
+        'PORT': '5432',              # Default PostgreSQL port
     }
+
 }
 
 
