4d4bbecef1a80dcca0ec547ed60f78a1a40e9c4f
Jared Bentil <jaredbentil@gmail.com>
2025-10-10 00:17:49 -0400

    Feature: user authentication

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -2,7 +2,6 @@
 
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from rest_framework.authtoken.models import Token
 
 CustomUser = get_user_model()
 
@@ -14,20 +13,39 @@ class UserSerializer(serializers.ModelSerializer):
         read_only_fields = ('followers', 'following')
 
 class RegisterSerializer(serializers.ModelSerializer):
-    """Serializer for user registration."""
+    """
+    Serializer for user registration.
+    Includes password confirmation and uses the create_user manager method.
+    """
+    # Explicitly adding a password confirmation field.
+    # It is write_only, so it will be used for validation but not shown in the API output.
+    password2 = serializers.CharField(style={'input_type': 'password'}, write_only=True)
+
     class Meta:
         model = CustomUser
-        fields = ('username', 'password', 'email', 'first_name', 'last_name')
-        extra_kwargs = {'password': {'write_only': True, 'style': {'input_type': 'password'}}}
+        fields = ('username', 'password', 'password2', 'email', 'first_name', 'last_name')
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def validate(self, data):
+        """
+        Check that the two password entries match.
+        """
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError({"password": "Passwords must match."})
+        return data
 
     def create(self, validated_data):
-        # Create user with a hashed password
-        user = CustomUser(
-            email=validated_data['email'],
+        """
+        Create and return a new user, using the create_user method.
+        """
+        # Using the improved create_user method which handles password hashing.
+        user = CustomUser.objects.create_user(
             username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
             first_name=validated_data.get('first_name', ''),
             last_name=validated_data.get('last_name', '')
         )
-        user.set_password(validated_data['password'])
-        user.save()
         return user
