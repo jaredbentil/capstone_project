0fe664b712d20a24462cd9a29e621c713b141f11
Jared Bentil <jaredbentil@gmail.com>
2025-10-10 00:14:58 -0400

    Feature: user authentication

---
+++ social_media_api/accounts/serializers.py
@@ -0,0 +1,33 @@
+# accounts/serializers.py
+
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+
+CustomUser = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer for the CustomUser model, used for retrieving user details."""
+    class Meta:
+        model = CustomUser
+        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'bio', 'profile_picture', 'followers', 'following')
+        read_only_fields = ('followers', 'following')
+
+class RegisterSerializer(serializers.ModelSerializer):
+    """Serializer for user registration."""
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'password', 'email', 'first_name', 'last_name')
+        extra_kwargs = {'password': {'write_only': True, 'style': {'input_type': 'password'}}}
+
+    def create(self, validated_data):
+        # Create user with a hashed password
+        user = CustomUser(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', '')
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
---
+++ social_media_api/accounts/views.py
@@ -0,0 +1,57 @@
+# accounts/views.py
+
+from django.contrib.auth import get_user_model
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from .serializers import UserSerializer, RegisterSerializer
+
+CustomUser = get_user_model()
+
+class RegisterView(generics.CreateAPIView):
+    """
+    API endpoint for user registration.
+    Returns user data and an auth token upon successful registration.
+    """
+    queryset = CustomUser.objects.all()
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = RegisterSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            "user": UserSerializer(user, context=self.get_serializer_context()).data,
+            "token": token.key
+        }, status=status.HTTP_201_CREATED)
+
+class CustomAuthToken(ObtainAuthToken):
+    """
+    API endpoint for user login.
+    Extends DRF's ObtainAuthToken to return user data alongside the token.
+    """
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'email': user.email
+        })
+
+class UserProfileView(generics.RetrieveUpdateAPIView):
+    """
+    API endpoint for retrieving and updating the authenticated user's profile.
+    """
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        # Returns the authenticated user's object
+        return self.request.user
