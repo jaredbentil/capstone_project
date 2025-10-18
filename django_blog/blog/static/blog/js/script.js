8c96718450dee5e2f3131205d60cb39448fb10e5
Jared Bentil <jaredbentil@gmail.com>
2025-10-18 07:23:14 -0400

    Delete django_blog directory

--- django_blog/blog/static/blog/js/script.js
+++
@@ -1,4 +0,0 @@
-// A simple script to confirm that the JS file is loaded
-document.addEventListener('DOMContentLoaded', (event) => {
-    console.log('DOM fully loaded and parsed. Django blog is ready!');
-});
